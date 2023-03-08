dd07c72997f09e86266444e8ee1fdd34d03a9bbc
Hemant Singh <hphero272@gamil.com>
2023-03-08 22:39:57 +0530

    admin update

--- AttendancePortal/accounts/admin.py
+++ AttendancePortal/accounts/admin.py
@@ -3,7 +3,7 @@ from .models import * #User , student
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(student)
-admin.site.register(teacher)
-admin.site.register(subject)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Subject)
 # admin.site.register(batch)
--- AttendancePortal/attendance/admin.py
+++ AttendancePortal/attendance/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(batch)
-admin.site.register(lecture)
+admin.site.register(Batch)
+admin.site.register(Lecture)
