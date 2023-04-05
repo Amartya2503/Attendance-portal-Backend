a184f0114feb6f81cccd0454cebce4a26dd94cda
Amartya2503 <amartyamishra2503@gmail.com>
2023-04-06 00:07:18 +0530

    updated admin.py

--- AttendancePortal/attendance/admin.py
+++ AttendancePortal/attendance/admin.py
@@ -3,3 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Attendance)
+admin.site.register(SubjectTeacher)
+admin.site.register(BatchStudent)
+admin.site.register(Lecture)
+admin.site.register(Batch)
